b09a63b4ed0d690fce30be05c01ecb6f60975a34
Jack Bradham <jackb@live.com>
2021-04-27 14:09:54 -0400

    Added top customers test gen

---
+++ POC v2/main_message_gen_command.cmd
@@ -0,0 +1,2 @@
+#command to create main test message Streaming_Data_Generator
+gcloud beta dataflow flex-template run main-v2-stream-fakes --template-file-gcs-location gs://dataflow-templates-us-central1/latest/flex/Streaming_Data_Generator --region us-central1 --parameters schemaLocation=gs://streamer-config/main_stream_config_v2.json,topic=projects/bandwidthstream/topics/InboundV2,qps=30
