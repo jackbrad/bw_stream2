598583b9b633145cfbc37957a117d3cf2751359f
Jack Bradham <jackb@live.com>
2021-04-27 18:03:44 -0400

    bug fixs

--- POC v2/create main dataflow job.cmd
+++ POC v2/create main dataflow job.cmd
@@ -1,1 +1,1 @@
-gcloud dataflow sql query 'SELECT * FROM pubsub.topic.bandwidthstream.InboundV2' --job-name dfsql-incomingv2-bq-a --region us-central1 --bigquery-write-disposition write-empty --bigquery-project bandwidthstream --bigquery-dataset insightsV2 --bigquery-table CORRELATED_MDR_LONG_TERM
+gcloud dataflow sql query 'SELECT * FROM pubsub.topic.bandwidthstream.InboundV2' --job-name dfsql-incomingv2-bq-a --region us-central1 --bigquery-write-disposition write-append --bigquery-project bandwidthstream --bigquery-dataset insightsV2 --bigquery-table CORRELATED_MDR_LONG_TERM
